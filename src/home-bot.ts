c1fccec033a3cc06362142d44fd9b1553bb96182
Puthipong <puthipong@bluebik.com>
2021-12-28 22:53:27 +0700

    Add bot function

--- src/home-bot.ts
+++ src/home-bot.ts
@@ -12,6 +12,7 @@ import * as line from '@line/bot-sdk';
 const CERT_PATH: string = process.env.CERT_PATH;
 const CHANNEL_ACCESS_TOKEN: string = process.env.CHANNEL_ACCESS_TOKEN;
 const CHANNEL_SECRET: string = process.env.CHANNEL_SECRET;
+const RESOURCE_FILE: string = process.env.RESOURCE_FILE;
 
 const config: line.MiddlewareConfig & line.ClientConfig = {
     channelAccessToken: CHANNEL_ACCESS_TOKEN,
@@ -20,11 +21,13 @@ const config: line.MiddlewareConfig & line.ClientConfig = {
 
 const app: Express = express();
 
-
 // Certificate
 const privateKey: string = fs.readFileSync(`${CERT_PATH}/privkey.pem`, 'utf8');
 const certificate: string = fs.readFileSync(`${CERT_PATH}/cert.pem`, 'utf8');
 const ca: string = fs.readFileSync(`${CERT_PATH}/chain.pem`, 'utf8');
+
+const rawdata: string = fs.readFileSync(RESOURCE_FILE, {encoding: 'utf8'});
+const memory: NodeJS.Dict<NodeJS.Dict<Array<string>>> = JSON.parse(rawdata);
 
 const credentials: SecureContextOptions = {
 	key: privateKey,
@@ -40,19 +43,82 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
-    });
+});
 
-    const client = new line.Client(config);
-    function handleEvent(event: line.WebhookEvent) {
+const client = new line.Client(config);
+function handleEvent(event: line.WebhookEvent) {
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null);
     }
 
-    return client.replyMessage(event.replyToken, {
+    const source: line.EventSource = event.source;
+    let id: string = null;
+    if(source.type == 'user') {
+        id = source.userId;
+    }
+    else if(source.type == 'group') {
+        id = source.groupId;
+    }
+
+    const replyToken: string = event.replyToken;
+    const message: string = event.message.text;
+
+    if(message.includes('แจ้งเตือน') || message.includes('เพิ่มรายการ')) {
+        const messages: Array<string> = message.split('-');
+        if(messages.length != 3) {
+            return reply(replyToken, 'incorrect fotmat: เพิ่มรายการ-ชื่อรายการ-รายละเอียด');
+        }
+
+        if(!memory[id]){
+            memory[id] = {};
+        }
+
+        if(!memory[id][messages[1]]){
+            memory[id][messages[1]] = [];
+        }
+
+        memory[id][messages[1]].push(messages[2]);
+        return reply(replyToken, 'บันทึกเรียบร้อย');
+    }
+    else if(message.includes('แสดงรายการ')) {
+        const messages: Array<string> = message.split('-');
+        if(messages.length != 2) {
+            return reply(replyToken, 'incorrect fotmat: แสดงรายการ-ชื่อรายการ');
+        }
+
+        return reply(replyToken, jsonStringify(memory[id][messages[1]]));
+    }
+
+    else if(message.includes('ลบรายการ')) {
+        const messages: Array<string> = message.split('-');
+        if(messages.length != 3 || !parseInt(messages[2])) {
+            return reply(replyToken, 'incorrect fotmat: ลบรายการ-ชื่อรายการ-ลำดับรายการ');
+        }
+
+        memory[id][messages[1]][parseInt(messages[2]) - 1] = undefined;
+
+        return reply(event.replyToken, 'ลบเรียบร้อย');
+    }
+
+    else {
+        reply(event.replyToken, message);
+    }
+}
+
+function jsonStringify(data: object): string {
+    return JSON.stringify(data, null, 4);
+}
+
+function reply(replyToken: string, text: string) {
+    return client.replyMessage(replyToken, {
         type: 'text',
-        text: event.message.text
+        text: text
     });
 }
+
+setTimeout(()=> {
+    fs.writeFileSync(RESOURCE_FILE, jsonStringify(memory));
+}, 3600000)
 
 // Starting both http & https servers
 const httpServer: http.Server = http.createServer(app);
