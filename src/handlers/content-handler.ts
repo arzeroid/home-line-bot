47ec0597c262b61da5f1c22de8a050c9e34e8690
Puthipong <puthipong@bluebik.com>
2023-06-12 21:31:55 +0700

    Add command to enable/disable auto save content

--- src/handlers/content-handler.ts
+++ src/handlers/content-handler.ts
@@ -2,11 +2,50 @@ import * as line from '@line/bot-sdk';
 import * as fs from 'fs';
 import lineBotClient from '../line-bot-client';
 import { Readable } from 'stream';
-
+import { jsonStringify } from '../utils';
 
 class ContentHandler {
+    protected isAutoSave: boolean = true;
 
     public handle = (event: line.MessageEvent): Promise<line.MessageAPIResponseBase> => {
+        const source: line.EventSource = event.source;
+        let id: string = null;
+        if (source.type == 'user') {
+            id = source.userId;
+        }
+        else if (source.type == 'group') {
+            id = source.groupId;
+        }
+
+        if (event.message.type == 'text') {
+            let message: line.TextEventMessage = <line.TextEventMessage>event.message;
+            let text: string = message.text;
+
+            switch (text) {
+                case 'แสดงคำสั่งทั้งหมด':
+                    return lineBotClient.pushMessage(id, jsonStringify([
+                        'enable auto save',
+                        'disable auto save',
+                        'auto save status'
+                    ]));
+
+                case 'enable auto save':
+                    this.isAutoSave = true;
+                    return lineBotClient.pushMessage(id, `Enable Auto Save Content`);
+
+                case 'disable auto save':
+                    this.isAutoSave = false;
+                    return lineBotClient.pushMessage(id, `Disable Auto Save Content`);
+
+                case 'auto save status':
+                    return lineBotClient.pushMessage(id, `Auto Save Content Status: ${this.isAutoSave}`);
+            }
+        }
+
+        if (!this.isAutoSave) {
+            return;
+        }
+
         let fileName: string = null;
 
         switch (event.message.type) {
@@ -26,16 +65,6 @@ class ContentHandler {
                 return;
         }
 
-
-        const source: line.EventSource = event.source;
-        let id: string = null;
-        if (source.type == 'user') {
-            id = source.userId;
-        }
-        else if (source.type == 'group') {
-            id = source.groupId;
-        }
-
         const ws: fs.WriteStream = fs.createWriteStream(fileName);
         return lineBotClient.getMessageContent(event.message.id).then((data: Readable) => {
             data.pipe(ws);
@@ -43,7 +72,7 @@ class ContentHandler {
                 ws.close();
             })
         }).then(() => {
-            return lineBotClient.pushMessage(id, `${event.message.type} is saved.`);
+            return lineBotClient.pushMessage(id, `${event.message.type} is saved as ${fileName}`);
         });
 
     }
